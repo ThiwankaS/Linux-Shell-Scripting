a76cb66adaca7c06337f79a3acf7e436b41548ca
Thiwanka Somachandra <thivankas@outlook.com>
2023-10-09 06:12:00 +0300

    File append using shell scripting

--- fileappend.sh
+++ fileappend.sh
@@ -1,17 +1,17 @@
 #! /bin/zsh
 
 echo -e "Enter file name :\c"
-read $file_name
+read file_name
 
 if [ -f $file_name ] 
 then
     if [ -w $file_name ]
         then
-            echo "Type some text data. To quit press ctrl + D."
+            echo -e "Type some text to enter.To quit press ctrl+D. :\c"
             cat >> $file_name
         else
             echo "You don't have write access to $file_name"
     fi
 else
-    echo "$file_name file not foud! "
+    echo "$file_name file not found! "
 fi
